bddb2b6231a646a087ff5eed8d8423ace3f7d098
Dhekra Rouatbi <dhekra.rouatbi@proxym-it.com>
2017-07-10 16:34:24 +0100

    Initial Commit

---
+++ BlinkingLabel/Classes/BlinkingLabel.swift
@@ -0,0 +1,21 @@
+//
+//  BlinkingLabel.swift
+//  Pods
+//
+//  Created by Dhekra Rouatbi on 10/07/2017.
+//
+//
+
+import UIKit
+
+class BlinkingLabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
